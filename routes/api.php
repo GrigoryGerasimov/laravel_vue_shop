505a56de23f532fb8b4ced9097546cc688a7f808
Grigory Gerasimov <grigorygerasimov@Grigorys-MacBook-Pro.local>
2023-08-09 16:57:34 +0200

    article show api route and controller added

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::group(['namespace' => 'App\Http\Controllers\Api'], function() {
     Route::group(['namespace' => 'Article', 'prefix' => 'articles'], function() {
         Route::get('/', 'IndexController')->name('api.articles.index');
+        Route::get('/{article}', 'ShowController')->name('api.articles.show');
     });
 
     Route::group(['namespace' => 'Category', 'prefix' => 'categories'], function() {
